489d8cb3d2335d6436482a0baee10a50c4bbb8de
Michael Nash <michael@spatialnetworks.com>
2019-07-23 17:22:11 -0400

    rna transcription

--- rna-transcription/src/rna_transcription.cr
+++ rna-transcription/src/rna_transcription.cr
@@ -1,1 +1,9 @@
-# Please implement your solution to rna-transcription in this file
+# class for RNA Transacription
+class RnaComplement
+  @@dna_chars = "GCTA"
+  @@rna_chars = "CGAU"
+
+  def self.of_dna(dna_strand)
+    dna_strand.tr(@@dna_chars, @@rna_chars)
+  end
+end
